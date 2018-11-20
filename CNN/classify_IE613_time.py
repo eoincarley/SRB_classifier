8a755630a4f852d5f4cd03bdf251647405350446
Eoin Carley <eoincarley@gmail.com>
2018-11-20 15:47:15 +0000

    Streamlined backsub

--- CNN/classify_IE613_time.py
+++ CNN/classify_IE613_time.py
@@ -32,19 +32,18 @@ from matplotlib import gridspec
 def add_label(taxis, prob, bursttype):
     ax1.text(taxis[-1], prob[-1], bursttype+' ('+str(round(prob[-1],2))+')', fontsize=8)
 
-
 def backsub(data):
-	# Devide each spectrum by the spectrum with the minimum standard deviation.
+    # Devide each spectrum by the spectrum with the minimum standard deviation.
     data = np.log10(data)
     data[np.where(np.isinf(data)==True)] = 0.0
     data_std = np.std(data, axis=0)
-    min_std_index=np.where(data_std==np.min( data_std[np.nonzero(data_std)] ))[0][0]
+    min_std_index = np.where(data_std==np.min( data_std[np.nonzero(data_std)] ))[0][0]
     min_std_spec = data[:, min_std_index]
-    data=np.transpose(np.divide(np.transpose(data), min_std_spec ))
-
+    nfreq = len(min_std_spec)
+    data = np.divide(data, min_std_spec.reshape(nfreq,1))
     #Alternative: Normalizing frequency channel responses using median of values.
-	#for sb in np.arange(data.shape[0]):
-	#   	data[sb, :] = data[sb, :]/np.mean(data[sb, :])
+    #for sb in np.arange(data.shape[0]):
+    #       data[sb, :] = data[sb, :]/np.mean(data[sb, :])
     return data
 
 def tf_label(graph, label_file):
